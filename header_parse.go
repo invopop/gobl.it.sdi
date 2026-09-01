da26bb6cab9adcfbf04c2b601bf6a9a0ba0cfbbd
Jose Rilla Álvarez <rilla@users.noreply.github.com>
2026-09-01 09:02:01 +0000

    Add third-party and customer issuer to the FatturaPA header

--- header_parse.go
+++ header_parse.go
@@ -1,7 +1,11 @@
 package fatturapa
 
 import (
+	sdi "github.com/invopop/gobl.it.sdi/addon"
 	"github.com/invopop/gobl/bill"
+	"github.com/invopop/gobl/cbc"
+	"github.com/invopop/gobl/org"
+	"github.com/invopop/gobl/tax"
 )
 
 func goblBillInvoiceAddHeader(inv *bill.Invoice, header *Header) {
@@ -12,6 +16,28 @@ func goblBillInvoiceAddHeader(inv *bill.Invoice, header *Header) {
 	inv.Supplier = goblOrgPartyFromSupplier(header.Supplier)
 	inv.Customer = goblOrgPartyFromCustomer(header.Customer)
 
+	goblBillInvoiceAddIssuer(inv, header)
+
 	// Need to do after customer is set
 	goblBillInvoiceAddTransmission(inv, header.TransmissionData)
+}
+
+func goblBillInvoiceAddIssuer(inv *bill.Invoice, header *Header) {
+	if header.IssuerType != "" {
+		inv.Tax = inv.Tax.MergeExtensions(tax.ExtensionsOf(cbc.CodeMap{
+			sdi.ExtKeyIssuerType: cbc.Code(header.IssuerType),
+		}))
+	}
+
+	if header.ThirdPartyIssuer == nil {
+		return
+	}
+
+	issuer := new(org.Party)
+	goblOrgPartyAddIdentity(issuer, header.ThirdPartyIssuer.Identity)
+
+	if inv.Ordering == nil {
+		inv.Ordering = new(bill.Ordering)
+	}
+	inv.Ordering.Issuer = issuer
 }
